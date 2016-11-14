1b9016b14a808201c03187a3c59ec07e67d48fad
HelloWorld20 <292701515@qq.com>
2016-11-14 15:53:25 +0800

    就想提交核心函数库--core.js

---
+++ core.js
@@ -0,0 +1,286 @@
+/**
+ * description：我的核心函数库
+ * author：weijianghong
+ * list：getClass;hasClass;addClass;removeClass;extend;queue;throttle;debounc;ajax;isArray;isFunction;isObject;isElement;
+ */
+
+/**
+ * [getClass 获取节点class]
+ * @param  {[type]} elem [dom节点]
+ * @return {[type]}      [class字符串]
+ */
+function getClass( elem ) {
+    //trim方法不兼容低版本浏览器。故getCLass系列都不支持
+    return (elem.className || (elem.getAttribute && elem.getAttribute('class'))).trim();
+}
+/**
+ * [hasClass 是否存在class]
+ * @param  {[type]}  elem  [dom节点]
+ * @param  {[type]}  value [要查询的class值]
+ * @return {Boolean}       [bool结果]
+ */
+function hasClass( elem, value ) {
+    if( !isElement( elem ) ) return false;
+    
+    var className = getClass( elem );
+
+    //首尾加上空格，就能直接用indexOf找到
+    className = ' ' + className + ' ';
+    value = ' ' + value + ' ';
+
+    if(className.indexOf(value) !== -1) {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * [addClass 添加class]
+ * @param {[type]} elem  [dom节点]
+ * @param {[type]} value [要添加的class值]
+ */
+function addClass( elem, value ) {
+    if( hasClass( elem, value ) ) return true;
+
+    var className;
+
+    className = getClass( elem );
+
+    className += ' ' + value;
+
+    elem.className = className;
+
+    return elem;
+}
+
+/**
+ * [removeClass 删除dom节点的class]
+ * @param  {[type]} elem  [dom节点]
+ * @param  {[type]} value [要删除的class]
+ * @return {[type]}       [dom节点]
+ */
+function removeClass( elem, value ) {
+    if( !hasClass ( elem, value ) ) return false;
+
+    var className;
+
+    className = getClass( elem );
+
+    //首尾加上空格就能直接用下标找到
+    className = ' ' + className + ' ';
+    value = ' ' + value + ' ';
+
+    className = className.replace(value, ' ');
+
+    elem.className = className;
+
+    return elem;
+}
+
+/**
+ * [extend 合并对象]
+ * @param  {[type]} target  [对象1，相同属性会被覆盖]
+ * @param  {[type]} options [对象2]
+ * @return {[type]}         [合并后的对象]
+ */
+function extend( target, options ) {
+    var result = new Object();  //return一个新对象，隔断引用
+
+    for( var property in target ) {
+
+        if( isObject( target[ property ] || isArray( target[ property ] ) ) ) {
+            //如果属性是对象和对象则递归调用，防止直接赋值引用。
+            result[ property ] = extend( {}, target[ property ] );  
+        } else {
+            result[ property ] = target[ property ];
+        }
+        
+    }
+
+    for( var property in options ) {
+
+        if( isObject( options[ property ] || isArray( target[ property ] ) ) ) {
+            //如果属性是对象和对象则递归调用，防止直接赋值引用。
+            result[ property ] = extend( {}, options[ property ] );
+        } else {
+            result[ property ] = options[ property ];
+        }
+        
+    }
+
+    return result;
+}
+
+/**
+ * [css 获取dom节点的指定样式值或者给dom节点添加样式]
+ * @param  {[type]} elem  [dom节点]
+ * @param  {[type]} style [样式名称]
+ * @param  {[type]} value [样式值，如果为空，则是读取样式]
+ * @return {[type]}       [dom节点]
+ */
+function css( elem, style, value ) {
+    if( !isElement( elem ) ) return false;
+
+    if( !value ) {
+        //两个参数读取参数
+        return window.getComputedStyle( elem )[style];
+    } else {
+        //三个参数写参数
+        elem.style[style] = value;
+    }
+
+    return elem;
+} 
+
+/**
+ * [queue 排队函数，用于把参数函数放到任务队列的最后。]
+ * @param  {[type]} func [要延迟执行的函数]
+ * @return {[type]}      [是否成功]
+ */
+function queue( func ) {
+    if( !isFunction( func ) ) return false;
+
+    setTimeout( function() {
+        try{
+            func();
+        } catch ( e ) {
+            throw new Error( 'Custom Error: queue error; ' + e );
+        }
+    }, 10 );
+
+    return true;
+}
+
+/**
+ * [throttle 函数节流，使用方法：throttle( function(){}, 50); ]
+ * @param  {[type]} func  [要节流执行的函数]
+ * @param  {[type]} delay [间隔时间]
+ * @return {[type]}       [undefined]
+ */
+var throttle = (function() {
+    var wating = false;     //是否在等待执行中
+    var timer;              //
+    var hasTimer = false;   //有没有设置定时器
+
+    return function( func, delay ) {
+
+        if( !isFunction( func ) ) return false;
+
+        var delay = delay || 200;
+
+        if( wating ) { 
+
+           if(!hasTimer) {
+                timer = setTimeout( function() { wating = false; hasTimer = false }, delay );
+                hasTimer = true;
+           }
+           //正在等待且已经设置过定时器则直接返回。
+           return;
+
+        } else {  
+
+            func();
+            clearTimeout( timer );
+            wating = true;
+
+        }
+    }
+})();
+
+/**
+ * [debounc 函数防抖，在频繁调时不会执行，只有停止调用后再执行。使用方法：debounc( function(){}, 50); ]
+ * @param {[type]} func [description]
+ */
+var debounc = ( function () {
+    var timer = null;
+    //这种方法虽然频繁设置和删除定时器，但是通过Profiles性能观测，性能没有太大影响。
+    return function ( func, delay ) {
+        if( !isFunction( func ) ) return false;
+
+        var delay = delay || 200;
+
+        clearTimeout( timer );
+        timer = setTimeout(function() {
+            func();
+        }, delay );
+    }
+})();
+
+
+
+function isArray( obj ) {
+    return Object.prototype.toString.call( obj ) === '[object Array]'
+}
+
+function isFunction( obj ) {
+    return Object.prototype.toString.call( obj ) === '[object Function]';
+}
+
+function isObject( obj ) {
+    return Object.prototype.toString.call( obj ) === '[object Object]';
+}
+
+//是否是html对象
+function isElement( obj ) {
+    return Object.prototype.toString.call(obj).indexOf('HTML') !== -1;
+}
+
+/**
+ * [ajax 网上抄的，]
+ * @param  {[type]} type    [get/post]
+ * @param  {[type]} url     [url]
+ * @param  {[type]} data    [参数对象]
+ * @param  {[type]} success [成功回调函数，返回200和304时执行]
+ * @param  {[type]} failed  [失败回调函数]
+ * @return {[type]}         [description]
+ */
+function ajax(type, url, data, success, failed){
+    // 创建ajax对象
+    var xhr = null;
+    if(window.XMLHttpRequest){
+        xhr = new XMLHttpRequest();
+    } else {
+        xhr = new ActiveXObject('Microsoft.XMLHTTP')
+    }
+ 
+    var type = type.toUpperCase();
+    // 用于清除缓存
+    var random = Math.random();
+ 
+    if(typeof data == 'object'){
+        var str = '';
+        for(var key in data){
+            str += key+'='+data[key]+'&';
+        }
+        data = str.replace(/&$/, '');
+    }
+ 
+    if(type == 'GET'){
+        if(data){
+            xhr.open('GET', url + '?' + data, true);
+        } else {
+            xhr.open('GET', url + '?t=' + random, true);
+        }
+        xhr.send();
+ 
+    } else if(type == 'POST'){
+        xhr.open('POST', url, true);
+        // 如果需要像 html 表单那样 POST 数据，请使用 setRequestHeader() 来添加 http 头。
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send(data);
+    }
+ 
+    // 处理返回数据
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+            if(xhr.status == 200 || xhr.status == 304){
+                success(xhr.responseText);
+            } else {
+                if(failed){
+                    failed(xhr.status);
+                }
+            }
+        }
+    }
+}
--- wap/readme.md
+++
@@ -1,4 +0,0 @@
-#送iPhone
-ichart+jquery+requirejs
-Oraginal:原始版本 artTemplate
-Rebuild:reactjs重写版本 reactjs
